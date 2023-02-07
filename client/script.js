d1d25af92aeda471645986e0f4050bb3ac231c16
karthik <67945756+SAIKARTHIK9999@users.noreply.github.com>
2023-02-07 15:56:34 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data from server -> AI's Response
-    const response = await fetch('http://localhost:5001',{
+    const response = await fetch('https://ask-ai-xa35.onrender.com',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
